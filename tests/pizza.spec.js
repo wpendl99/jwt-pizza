a840cd94e9a17162de84bad3edaad47dd30b0ce4
William Keith Pendleton <wkp23@byu.edu>
2024-10-03 16:05:16 -0600

    update(pizza.spec.js) added login test

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -5,3 +5,17 @@ test("home page", async ({ page }) => {
 
     expect(await page.title()).toBe("JWT Pizza");
 });
+
+test("login", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    await page.getByRole("link", { name: "Login" }).click();
+    await page.getByPlaceholder("Email address").click();
+    await page.getByPlaceholder("Email address").fill("dummy@jwt.com");
+    await page.getByPlaceholder("Password").fill("dummy");
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(
+        page.getByRole("link", { name: "d", exact: true })
+    ).toBeVisible();
+    await page.getByRole("link", { name: "d", exact: true }).click();
+    await expect(page.getByText("dummy@jwt.com")).toBeVisible();
+});
