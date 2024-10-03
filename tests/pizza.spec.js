2174fce1678242226d3cb385d9bf600405534167
William Keith Pendleton <wkp23@byu.edu>
2024-10-03 16:26:47 -0600

    update(pizza.spec.js) update login test

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -7,15 +7,33 @@ test("home page", async ({ page }) => {
 });
 
 test("login", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
+    await page.route("*/**/api/auth", async (route) => {
+        const loginReq = { email: "dummy@jwt.com", password: "dummy" };
+        const loginRes = {
+            user: {
+                id: 191,
+                name: "dummy dummy",
+                email: "dummy@jwt.com",
+                roles: [{ role: "diner" }],
+            },
+            token: "abcdef",
+        };
+        expect(route.request().method()).toBe("PUT");
+        expect(route.request().postDataJSON()).toMatchObject(loginReq);
+        await route.fulfill({ json: loginRes });
+    });
+
+    await page.goto("/");
     await page.getByRole("link", { name: "Login" }).click();
     await page.getByPlaceholder("Email address").click();
     await page.getByPlaceholder("Email address").fill("dummy@jwt.com");
     await page.getByPlaceholder("Password").fill("dummy");
     await page.getByRole("button", { name: "Login" }).click();
     await expect(
-        page.getByRole("link", { name: "d", exact: true })
+        page.getByRole("link", { name: "dd", exact: true })
     ).toBeVisible();
-    await page.getByRole("link", { name: "d", exact: true }).click();
+    await page.getByRole("link", { name: "dd", exact: true }).click();
     await expect(page.getByText("dummy@jwt.com")).toBeVisible();
+    await expect(page.getByText("diner", { exact: true })).toBeVisible();
+    await expect(page.getByText("dummy dummy")).toBeVisible();
 });
