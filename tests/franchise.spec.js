d3bdd52e9ffa0a954adf5d071a4cd37a65439efe
William Keith Pendleton <wkp23@byu.edu>
2024-10-08 22:18:00 -0600

    update(franchise.spec.js) added create store and delete store

--- tests/franchise.spec.js
+++ tests/franchise.spec.js
@@ -3,17 +3,21 @@ import { v4 as uuidv4 } from "uuid";
 
 let sharedID;
 let sharedName;
+let sharedNameInitials;
 let sharedEmail;
 let sharedPassword;
 let sharedFranchiseName;
+let sharedStoreName;
 
 // Before All
 test.beforeAll(async () => {
     sharedID = uuidv4();
     sharedName = "Test Franchise";
+    sharedNameInitials = "TF";
     sharedEmail = `testfranchise_${uuidv4()}@jwt.com`;
     sharedPassword = "testpassword";
     sharedFranchiseName = "Mama Ricci's kitchen";
+    sharedStoreName = "Mama Ricci's kitchen - Store";
 
     // Create a new user
     const registerReq = { email: sharedEmail, password: sharedPassword };
@@ -64,14 +68,9 @@ async function loginFranchisee(page, email, password) {
     await page.getByPlaceholder("Password").fill(password);
 
     await page.getByRole("button", { name: "Login" }).click();
-}
 
-async function createStore(page, name) {}
-
-// Test Franchisee login
-test("login", async ({ page }) => {
-    await loginFranchisee(page, sharedEmail, sharedPassword);
     await expect(page.getByRole("link", { name: "TF" })).toBeVisible();
+
     await page
         .getByLabel("Global")
         .getByRole("link", { name: "Franchise" })
@@ -80,4 +79,93 @@ test("login", async ({ page }) => {
         page.getByRole("link", { name: "franchise-dashboard" })
     ).toBeVisible();
     await expect(page.getByText("Mama Ricci's kitchen")).toBeVisible();
+}
+
+async function createStore(page, name) {
+    await page.route("*/**/api/franchise/*/store", async (route) => {
+        if (route.request().method() === "POST") {
+            const createStoreReq = { name };
+            const createStoreRes = {
+                id: uuidv4(),
+                franchiseId: sharedID,
+                name,
+            };
+            expect(route.request().method()).toBe("POST");
+            expect(route.request().postDataJSON()).toMatchObject(
+                createStoreReq
+            );
+            await route.fulfill({ json: createStoreRes });
+        }
+    });
+    await page.route("*/**/api/franchise/*", async (route) => {
+        if (route.request().method() === "GET") {
+            const getFranchiseRes = [
+                {
+                    id: sharedID,
+                    name: sharedFranchiseName,
+                    admins: [
+                        {
+                            id: sharedID,
+                            name: sharedName,
+                            email: sharedEmail,
+                        },
+                    ],
+                    stores: [
+                        {
+                            id: sharedID,
+                            name: name,
+                            totalRevenue: 3.14,
+                        },
+                    ],
+                },
+            ];
+            await route.fulfill({ json: getFranchiseRes });
+        }
+    });
+    await page.route("*/**/api/franchise/*/store/*", async (route) => {
+        if (route.request().method() === "DELETE") {
+            const deleteStoreRes = { message: "store deleted" };
+            expect(route.request().method()).toBe("DELETE");
+            await route.fulfill({ json: deleteStoreRes });
+        }
+    });
+
+    await page
+        .getByLabel("Global")
+        .getByRole("link", { name: "Franchise" })
+        .click();
+    await page.getByRole("button", { name: "Create store" }).click();
+    await expect(
+        page.getByRole("link", { name: "create-store" })
+    ).toBeVisible();
+    await expect(page.getByText("Create store")).toBeVisible();
+    await page.getByPlaceholder("store name").fill(name);
+    await page.getByRole("button", { name: "Create" }).click();
+    await expect(page.getByText(name)).toBeVisible();
+}
+
+// Test Franchisee login
+test("login", async ({ page }) => {
+    await loginFranchisee(page, sharedEmail, sharedPassword);
+});
+
+// Test Store creation
+test("create store", async ({ page }) => {
+    await loginFranchisee(page, sharedEmail, sharedPassword);
+    await createStore(page, sharedStoreName);
+});
+
+// Test Store deletion
+test("delete store", async ({ page }) => {
+    await loginFranchisee(page, sharedEmail, sharedPassword);
+    await createStore(page, sharedStoreName);
+
+    // Delete store
+    await page.getByRole("button", { name: "Close" }).click();
+    await expect(page.getByRole("link", { name: "close-store" })).toBeVisible();
+    await expect(page.getByText("Sorry to see you go")).toBeVisible();
+    await page.getByRole("button", { name: "Close" }).click();
+    await expect(
+        page.getByText("Mama Ricci's kitchen", { exact: true })
+    ).toBeVisible();
 });
